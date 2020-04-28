105ba681da764a4972a5248063b634471054c12e
Sujith Quintelier <sujith.quintelier@arxus.eu>
2020-04-28 10:45:32 +0200

    Module
    Resource Group Module

--- terraform-aks-k8s/k8s.tf
+++ terraform-aks-k8s/k8s.tf
@@ -1,3 +1,10 @@
+module "acme-rg" {
+    source   = "./modules/acme-rg"
+    name     = var.resource_group_name
+    location = var.location
+}
+
+
 resource "azurerm_resource_group" "k8s" {
     name     = var.resource_group_name
     location = var.location
---
+++ terraform-aks-k8s/modules/acme-rg/variables.tf
@@ -0,0 +1,9 @@
+variable name {
+    default = "azure-k8stest"
+    description = "Resource Group Name"
+}
+
+variable location {
+    default = "WestEurope"
+    description = "Resource Group Location"
+}
